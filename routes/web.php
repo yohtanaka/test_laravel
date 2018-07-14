f82cc3cfa8e5d88151ff7c9b8ee7d01ca44104ec
yohei tanaka <oceanyoh@gmail.com>
2018-07-15 00:39:45 +0900

    Make post-controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 Route::get('/', function () { return view('index'); });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/post', 'PostController@index');
+Route::get('/post/create', 'PostController@create');
+Route::get('/post/confirm', 'PostController@confirm');
+Route::post('/post/store', 'PostController@store');
+Route::get('/post/show/{id}', 'PostController@index');
+Route::get('/post/edit/{id}', 'PostController@edit');
+Route::get('/post/update/{id}', 'PostController@update');
+Route::post('/post/destroy/{id}','PostController@destroy');
