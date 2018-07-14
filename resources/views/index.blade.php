adb0653a8ac3f04e39e90230cdebf7d137cccf49
vagrant <vagrant@localhost.localdomain>
2018-07-14 19:41:07 +0900

    Make top page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.common')
+
+@section('title', 'Laravel')
+
+@section('content')
+<div class="clearfix">
+  <img class="left" src="images/logo.png" alt="">
+  <h1 class="title left">Laravel</h1>
+</div>
+<h2>機能のテスト実装</h2>
+<ul>
+  <li></li>
+</ul>
+@endsection
---
+++ resources/views/layouts/common.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Test: @yield('title')</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
