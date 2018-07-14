2aed73f81d553626d9dd1eb282f578e141408adf
yohei tanaka <oceanyoh@gmail.com>
2018-07-15 00:22:33 +0900

    Update view files

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -1,8 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Test: @yield('title')</title>
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
   <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
   @include('layouts.header')
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,5 @@
 <footer>
   <p>thank you for visiting me!</p>
+  <li><a href="https://www.facebook.com/yohei.tanaka.900/" class="facebook"><i class="icon-facebook"></i></a></li>
   <p class="copy-right">&copy; 2018 Yohei Tanaka</p>
 </footer>
