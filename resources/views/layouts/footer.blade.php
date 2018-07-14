b0b80c57ee3db6bb792baeb74ac83f826e5ccf09
yohvagrant <vagrant@localhost.localdomain>
2018-07-15 02:53:45 +0900

    Edit footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-  <p>thank you for visiting me!</p>
-  <li><a href="https://www.facebook.com/yohei.tanaka.900/" class="facebook"><i class="icon-facebook"></i></a></li>
+  <p class="copy-right">thank you for visiting me! feel free to contact me</p>
+  <i class="fa fa-cog"></i>icon
   <p class="copy-right">&copy; 2018 Yohei Tanaka</p>
 </footer>
