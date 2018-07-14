813aa52a5a40801b1c009f3be6d9920432aa6dc3
yohei tanaka <oceanyoh@gmail.com>
2018-07-14 21:12:48 +0900

    Add login function

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,4 +3,13 @@
     <img class="left" src="images/logo.png" alt="">
     <h1 class="left">Laravel</h1>
   </a>
+  <a href="/login" class="menu">ログイン</a>
+  <a href="/register" class="menu">新規登録</a>
+  <a href="" class="menu">
+    <?php
+    $user = \Auth::user();
+    if ($user) {
+      echo "Hello $user->name";
+    }?>
+  </a>
 </header>
