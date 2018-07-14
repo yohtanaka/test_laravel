228d18f109c32fdbf7e36703a953508afcc02914
Yohei_Vagrant <vagrant@localhost.localdomain>
2018-07-15 02:25:05 +0900

    Fix details

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header class="clearfix">
   <a href="/">
-    <img class="left" src="images/logo.png" alt="">
+    <img class="left" src="/images/logo.png" alt="">
     <h1 class="left">Laravel</h1>
   </a>
   <a href="/login" class="menu">ログイン</a>
