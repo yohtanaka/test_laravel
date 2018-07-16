f9363e64f6998a2e0fa866806e03857ea3d46453
yohei tanaka <oceanyoh@gmail.com>
2018-07-16 18:18:04 +0900

    Add logout function

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,15 +1,21 @@
+<?php $user = Auth::user(); ?>
 <header class="clearfix">
   <a href="/">
     <img class="left" src="/images/logo.png" alt="">
     <h1 class="left">Laravel<span> でららべる？</span></h1>
   </a>
-  <a href="/login" class="menu">ログイン</a>
-  <a href="/register" class="menu">新規登録</a>
-  <a href="" class="menu">
-    <?php
-    $user = \Auth::user();
-    if ($user) {
-      echo "Hello $user->name";
-    }?>
-  </a>
+  @if($user)
+    <a class="menu" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+        @csrf
+    </form>
+    <a href="" class="menu">
+      @if($user)
+        こんにちは！{{$user->name}}さん
+      @endif
+    </a>
+  @else
+    <a href="{{ route('login') }}" class="menu">ログイン</a>
+    <a href="{{ route('register') }}" class="menu">新規登録</a>
+  @endif
 </header>
