46802b7d8232cdfcc05de463ffa2cfac40f58784
yohei tanaka <oceanyoh@gmail.com>
2018-07-20 01:16:42 +0900

    Update view file

--- resources/views/csv/csv.blade.php
+++ resources/views/csv/csv.blade.php
@@ -4,14 +4,17 @@
 
 @section('content')
 <h2>CSV機能</h2>
-<ul>
-  <li>
-  <form action="/csv/import" method="post" enctype="multipart/form-data" accept-charset="utf-8">
-    {{ csrf_field() }}
-    <input type="file" name="csvFile">
-    <input type="submit" name="" value="インポート">
-  </form>
-  </li>
-  <li><a href="/csv/export">CSVファイルをエクスポート</a></li>
-</ul>
+  <ul>
+    <li>SCVファイルインポート</li>
+    <form action="/csv/import" method="post" enctype="multipart/form-data" accept-charset="utf-8">
+      {{ csrf_field() }}
+      <label for="csvFile">
+        <p class="btn btn-laravel">インポートするファイルを選択</p>
+        <input type="file" id="csvFile" name="csvFile">
+      </label>
+      <input class="btn btn-laravel" type="submit" name="" value="インポートし表示">
+    </form>
+    <li>CSVファイルエクスポート</li>
+    <a class="btn btn-laravel" href="/csv/export">オススメのお店情報をCSVファイルとしてエクスポート</a>
+  </ul>
 @endsection
