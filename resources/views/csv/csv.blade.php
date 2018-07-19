7c9e690dfb0490c6e8766c1317de37932ee13abf
yohei tanaka <oceanyoh@gmail.com>
2018-07-19 23:24:42 +0900

    Edit view files

--- resources/views/csv/csv.blade.php
+++ resources/views/csv/csv.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
 <h2>CSV機能</h2>
-<p></p>
-<a href="/csv/export">CSVファイルのエクスポート</a>
+<ul>
+  <li><a href="/csv/import">CSVファイルをインポート</a></li>
+  <li><a href="/csv/export">CSVファイルをエクスポート</a></li>
+</ul>
 @endsection
--- resources/views/csv/export.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.common')
-
-@section('title', 'CSV')
-
-@section('content')
-<h2>CSV機能</h2>
-<p>CSVファイルのエクスポートが完了しました</p>
-<a href="/csv">CSV機能トップに戻る</a>
-@endsection
