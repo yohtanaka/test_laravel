a632d152352b222ea914d4f79490906a6b7ce09f
yohei tanaka <oceanyoh@gmail.com>
2018-07-22 01:44:40 +0900

    Make display-tweets function

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -9,7 +9,17 @@ class TwitterController extends Controller
 {
   public function index()
   {
+    $tck = env("TWITTER_CONSUMER_KEY");
+    $tcs = env("TWITTER_CONSUMER_SECRET");
+    $tat = env("TWITTER_ACCESS_TOKEN");
+    $tac = env("TWITTER_ACCESS_TOKEN_SECRET");
 
+    $connection = new TwitterOAuth($tck, $tcs, $tat, $tac);
+    $contents = $connection->get(
+      "statuses/home_timeline",
+      array("count" => 20)
+    );
+    return view('twitter.index', compact('contents'));
   }
 
   public function create()
@@ -20,8 +30,5 @@ class TwitterController extends Controller
   public function store(Request $request)
   {
 
-    );
   }
 }
-
-
