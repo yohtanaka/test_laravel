0c56e65f014598d22803cb8b52ce82b6ad8f70e1
yohei tanaka <oceanyoh@gmail.com>
2018-07-26 00:26:04 +0900

    Make countdown function

---
+++ public/js/timer.js
@@ -0,0 +1,24 @@
+$(function() {
+  countdown();
+});
+
+function countdown() {
+  var _year  = Number($('#year').data('date'));
+  var _month = Number($('#month').data('date'));
+  var _day   = Number($('#day').data('date'));
+  var startTime = new Date();
+  var endTime   = new Date(`${_year},${_month},${_day}, 00:00:00`);
+  var diff  = endTime - startTime;
+  var times = 24 * 60 * 60 * 1000;
+  var day   = Math.floor(diff / times);
+  var hour  = Math.floor(diff % times / (60 * 60 * 1000));
+  var min   = Math.floor(diff % times / (60 * 1000)) % 60;
+  var sec   = Math.floor(diff % times / 1000) % 60 % 60;
+  var ms    = Math.floor(diff % times / 10) % 100;
+  if(diff > 0){
+    $("#timer").text(day + '日' + hour + '時間' + min + '分' + sec +'秒' + ms);
+    setTimeout('countdown()', 10);
+  } else {
+    $("#timer").text('*終了のメッセージ');
+  }
+}
