bf4e002ad4c652d356f3c49e627665708ab0e1d6
yohei tanaka <oceanyoh@gmail.com>
2018-07-28 14:03:32 +0900

    Add post-photo form

--- resources/views/photo/create.blade.php
+++ resources/views/photo/create.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 <h2>写真投稿機能</h2>
-<p>写真を選択してください</p>
+<p>写真を選択</p>
+<form action="/photo/confirm" method="post" enctype="multipart/form-data" accept-charset="utf-8">
+  {{ csrf_field() }}
+  <label for="imageFile">
+    <p class="btn btn-laravel">投稿する写真を選択</p>
+    <input type="file" id="imageFile" class="file" name="photo">
+  </label>
+  <input class="btn btn-laravel" type="submit" value="確認画面へ">
+  <div id="filename"></div>
+</form>
 <a href="/photo">投稿一覧にもどる</a>
 @endsection
--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -4,5 +4,7 @@
 
 @section('content')
 <h2>写真投稿機能</h2>
+<a href="/photo/create">新規投稿</a>
+<hr>
 <p>投稿写真一覧</p>
 @endsection
