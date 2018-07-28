36791bd3024bfb7ac2c0d275cf15d81031b030cf
yohei tanaka <oceanyoh@gmail.com>
2018-07-28 19:58:08 +0900

    Update css

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -7,12 +7,9 @@
 <a href="/photo/create">新規投稿</a>
 <hr>
 <p>投稿写真一覧</p>
-@foreach($photos as $photo)
-{{ $photo['title'] }}
-<br>
-{{ $photo['comment'] }}
-<br>
-<img src="{{ $photo['image'] }}">
-<br>
-@endforeach
+<div class="photo__wrapper">
+  @foreach($photos as $photo)
+  <img src="{{ $photo['image'] }}" class="photo">
+  @endforeach
+</div>
 @endsection
