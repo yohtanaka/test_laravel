cea6120e99a784120dec45f6ec350d7a55bdf326
yohei tanaka <oceanyoh@gmail.com>
2018-07-28 15:40:14 +0900

    Set association

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+  protected $fillable = [
+    'user_id', 'title', 'comment'
+  ];
+
+  public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
 }
