b639a7003e22c864634ac5b3e1f1c814153a9d4a
yohei tanaka <oceanyoh@gmail.com>
2018-07-28 13:43:16 +0900

    Add view files

---
+++ resources/views/photo/confirm.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.common')
+
+@section('title', 'Photo')
+
+@section('content')
+<h2>写真投稿機能</h2>
+<p>投稿する写真はこちらでよろしいですか？</p>
+<a href="/photo">投稿一覧にもどる</a>
+@endsection
---
+++ resources/views/photo/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.common')
+
+@section('title', 'Photo')
+
+@section('content')
+<h2>写真投稿機能</h2>
+<p>投稿写真一覧</p>
+@endsection
