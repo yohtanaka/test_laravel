7f917766d3d44d80f5ab87fcb42af0814206fd3f
yohei tanaka <oceanyoh@gmail.com>
2018-07-30 23:47:43 +0900

    Fix css

--- resources/views/photo/show.blade.php
+++ resources/views/photo/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <h2>写真投稿機能</h2>
 <p>写真情報</p>
-<img src="{{ $photo['image'] }}">
+<img class="photo__large" src="{{ $photo['image'] }}">
 <ul>
   @if ($photo['user_id'])
     <li>投稿者: {{ $photo->user['name'] }}</li>
@@ -17,5 +17,6 @@
     <li>コメント: {{ $photo['comment'] }}</li>
   @endif
 </ul>
+<hr>
 <a href="/photo">投稿一覧にもどる</a>
 @endsection
