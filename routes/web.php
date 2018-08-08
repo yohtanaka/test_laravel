5ce5328b6ea9144bd23848e1ed3fadf6196fc19b
yohei tanaka <oceanyoh@gmail.com>
2018-08-08 23:25:54 +0900

    Add prefix to routing

--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,56 @@ Route::get('/', function () { return view('index'); });
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/post', 'PostController@index');
-Route::get('/post/create', 'PostController@create');
-Route::post('/post/confirm', 'PostController@confirm');
-Route::post('/post/store', 'PostController@store');
-Route::get('/post/{id}', 'PostController@show');
-Route::get('/post/{id}/edit', 'PostController@edit');
-Route::put('/post/{id}', 'PostController@update');
-Route::delete('/post/{id}','PostController@destroy');
+Route::prefix('/post')->group(function() {
+    Route::get('/', 'PostController@index');
+    Route::get('/create', 'PostController@create');
+    Route::post('/confirm', 'PostController@confirm');
+    Route::post('/store', 'PostController@store');
+    Route::get('/{id}', 'PostController@show');
+    Route::get('/{id}/edit', 'PostController@edit');
+    Route::put('/{id}', 'PostController@update');
+    Route::delete('/{id}','PostController@destroy');
+});
 
-Route::get('/mail', 'MailController@index');
-Route::post('/mail/confirm', 'MailController@confirm');
-Route::post('/mail/sent', 'MailController@sent');
+Route::prefix('/mail')->group(function() {
+    Route::get('/', 'MailController@index');
+    Route::post('/confirm', 'MailController@confirm');
+    Route::post('/sent', 'MailController@sent');
+});
 
-Route::get('/csv', 'CsvController@index');
-Route::post('/csv/import', 'CsvController@import');
-Route::get('/csv/export', 'CsvController@export');
+Route::prefix('/csv')->group(function() {
+  Route::get('/', 'CsvController@index');
+  Route::post('/import', 'CsvController@import');
+  Route::get('/export', 'CsvController@export');
+});
 
-Route::get('/slack', 'SlackController@index');
-Route::post('/slack', 'SlackController@post');
+Route::prefix('/slack')->group(function() {
+    Route::get('/', 'SlackController@index');
+    Route::post('/', 'SlackController@post');
+});
 
-Route::get('/twitter', 'TwitterController@index');
-Route::get('/twitter/create', 'TwitterController@create');
-Route::post('/twitter/store', 'TwitterController@store');
-Route::get('/twitter/search', 'TwitterController@search');
-Route::post('/twitter/show_result', 'TwitterController@showResult');
+Route::prefix('/post')->group(function() {
+    Route::get('/twitter', 'TwitterController@index');
+    Route::get('/twitter/create', 'TwitterController@create');
+    Route::post('/twitter/store', 'TwitterController@store');
+    Route::get('/twitter/search', 'TwitterController@search');
+    Route::post('/twitter/show_result', 'TwitterController@showResult');
+});
 
-Route::get('/birthday', 'BirthdayController@index');
-Route::post('/birthday/search', 'BirthdayController@search');
+Route::prefix('/birthday')->group(function() {
+    Route::get('/', 'BirthdayController@index');
+    Route::post('/search', 'BirthdayController@search');
+});
 
-Route::get('/timer', 'TimerController@index');
-Route::post('/timer/birthday', 'TimerController@birthday');
+Route::prefix('/timer')->group(function() {
+    Route::get('/', 'TimerController@index');
+    Route::post('/birthday', 'TimerController@birthday');
+});
 
-Route::get('/photo', 'PhotoController@index');
-Route::get('/photo/create', 'PhotoController@create');
-Route::post('/photo/confirm', 'PhotoController@confirm');
-Route::post('/photo/store', 'PhotoController@store');
-Route::get('/photo/{id}', 'PhotoController@show');
+Route::prefix('/photo')->group(function() {
+    Route::get('/', 'PhotoController@index');
+    Route::get('/create', 'PhotoController@create');
+    Route::post('/confirm', 'PhotoController@confirm');
+    Route::post('/store', 'PhotoController@store');
+    Route::get('/{id}', 'PhotoController@show');
+});
