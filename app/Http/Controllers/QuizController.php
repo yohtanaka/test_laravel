d17efa9730f90a35b4d5a7fe8fb2b389f5ce76df
yohei tanaka <oceanyoh@gmail.com>
2018-08-13 22:16:56 +0900

    Add actions to quiz-controller

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -6,8 +6,58 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    public function index()
-    {
-      return view('quiz.index');
+  private $_quizSet = [];
+
+  public function __construct()
+  {
+    $this->_setup();
+    if (!isset($_SESSION['current_num'])) {
+      $_SESSION['current_num'] = 0;
     }
+  }
+
+  public function getCurrentQuiz()
+  {
+    return $this->_quizSet[$_SESSION['current_num']];
+  }
+
+  private function _setup()
+  {
+    $this->_quizSet[] = [
+      'q' => 'PHPのフレームワークではないものはどれ？',
+      'a' => [
+        'Django',
+        'Laravel',
+        'CakePHP',
+        'Symfony'
+      ]
+    ];
+    $this->_quizSet[] = [
+      'q' => 'PHPのパッケージ管理システムといえば？',
+      'a' => [
+        'Composer',
+        'Writer',
+        'Singer',
+        'Engineer'
+      ]
+    ];
+    $this->_quizSet[] = [
+      'q' => 'Laravelで使用される言語といえば？',
+      'a' => [
+        'PHP',
+        'Ruby',
+        'Python',
+        'Java'
+      ]
+    ];
+  }
+
+  public function index()
+  {
+    $quiz = new QuizController();
+    $data = $quiz->getCurrentQuiz();
+    shuffle($data['a']);
+
+    return view('quiz.index', compact('data'));
+  }
 }
--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -5,12 +5,11 @@
 @section('content')
 <h2>クイズ機能</h2>
 <div id="container">
-  <h3>Q. What is A?</h3>
+  <h3>Q. {{ $data['q'] }}</h3>
   <ul>
-    <li class="answer">A0</li>
-    <li class="answer">A1</li>
-    <li class="answer">A2</li>
-    <li class="answer">A3</li>
+    @foreach ($data['a'] as $a)
+      <li class="answer">{{ $a }}</li>
+    @endforeach
   </ul>
   <div id="btn" class="btn btn-laravel disabled">Next Question</div>
 </div>
