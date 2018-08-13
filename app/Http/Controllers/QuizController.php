b7cf11845a7a0f06a45e84e2196e295bbf952d20
yohei tanaka <oceanyoh@gmail.com>
2018-08-13 21:37:40 +0900

    Make quiz view

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -8,6 +8,6 @@ class QuizController extends Controller
 {
     public function index()
     {
-
+      return view('quiz.index');
     }
 }
--- resources/views/quiz/index.blade.php
+++ resources/views/quiz/index.blade.php
@@ -4,4 +4,14 @@
 
 @section('content')
 <h2>クイズ機能</h2>
+<div id="container">
+  <h3>Q. What is A?</h3>
+  <ul>
+    <li class="answer">A0</li>
+    <li class="answer">A1</li>
+    <li class="answer">A2</li>
+    <li class="answer">A3</li>
+  </ul>
+  <div id="btn" class="btn btn-laravel disabled">Next Question</div>
+</div>
 @endsection
