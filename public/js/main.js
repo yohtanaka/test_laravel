fc6e9abcfe8a18daaa7d1fe8c3f4fe34944ddc36
yohei tanaka <oceanyoh@gmail.com>
2018-09-18 23:48:08 +0900

    Update form

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,66 @@
 $(function(){
-  $('#mailon').click(function() {
-    $('#mailto').html('<input class="form-control" type="email" name="mailto">');
-  })
+	$('#mailon').click(function() {
+		$('#mailto').html('<input class="form-control" type="email" name="mailto">');
+	})
 
-  $('#mailoff').click(function() {
-    $('#mailto').html('');
-  })
+	$('#mailoff').click(function() {
+		$('#mailto').html('');
+	})
 
-  $("#slack").on("keypress", "#slacktext", function(e) {
-    if (e.keyCode == 13) {
-      if (e.shiftKey) {
-        $.noop();
-      } else if ($("#slacktext").val().replace(/\s/g, "").length > 0) {
-        e.preventDefault();
-        $("#slackform").submit();
-      }
-    } else {
-      $.noop();
-    }
-  });
+	$("#slack").on("keypress", "#slacktext", function(e) {
+		if (e.keyCode == 13) {
+			if (e.shiftKey) {
+				$.noop();
+			} else if ($("#slacktext").val().replace(/\s/g, "").length > 0) {
+				e.preventDefault();
+				$("#slackform").submit();
+			}
+		} else {
+			$.noop();
+		}
+	});
 
-  $('.file').on('change', function() {
-    var fileName = $(this).prop('files')[0].name;
-    $('#filename').html('選択中のファイル: ' + fileName);
-  });
+	$('.file').on('change', function() {
+		var fileName = $(this).prop('files')[0].name;
+		$('#filename').html('選択中のファイル: ' + fileName);
+	});
 
-  $('form').on('change', 'input[type="file"]', function(e) {
-    var file = e.target.files[0],
-        reader = new FileReader(),
-        $preview = $("#fileimage");
-        t = this;
-    if(file.type.indexOf("image") < 0){
-      return false;
-    }
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        $preview.append($('<img>').attr({
-                  src: e.target.result,
-                  height: "300px",
-                  class: "preview",
-                  title: file.name
-              }));
-      };
-    })(file);
-    reader.readAsDataURL(file);
-    $('#title').html('タイトル<input class="form-control post-table__short" type="text" name="title" placeholder="任意">')
-    $('#comment').html('コメント<input class="form-control post-table__short" type="text" name="comment" placeholder="任意">')
-    $('#send').html('<input class="btn btn-laravel" type="submit" value="確認画面へ">');
-  });
+	$('form').on('change', 'input[type="file"]', function(e) {
+		var file = e.target.files[0],
+			reader = new FileReader(),
+			$preview = $("#fileimage");
+			t = this;
+		if(file.type.indexOf("image") < 0){
+			return false;
+		}
+	reader.onload = (function(file) {
+		return function(e) {
+			$preview.empty();
+			$preview.append($('<img>').attr({
+				src: e.target.result,
+				height: "300px",
+				class: "preview",
+				title: file.name
+			}));
+		};
+	})(file);
+	reader.readAsDataURL(file);
+		$('#title').html('タイトル<input class="form-control post-table__short" type="text" name="title" placeholder="任意">')
+		$('#comment').html('コメント<input class="form-control post-table__short" type="text" name="comment" placeholder="任意">')
+		$('#send').html('<input class="btn btn-laravel" type="submit" value="確認画面へ">');
+	});
 
-  $('.box').on('click', function() {
-    $('.box').removeClass('vote-selected');
-    $(this).addClass('vote-selected');
-    $('#answer').val($(this).data('id'));
-  });
+	$('.box').on('click', function() {
+		$('.box').removeClass('vote-selected');
+		$(this).addClass('vote-selected');
+		$('#answer').val($(this).data('id'));
+	});
+
+	$('#btn').on('click', function() {
+		if ($('#answer').val() === '') {
+			alert('ひとつを選んでください');
+		} else {
+			$('form').submit();
+		}
+	});
 })
