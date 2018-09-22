34768e13dee672b013677a0d5fce03381a2e7609
yohei tanaka <oceanyoh@gmail.com>
2018-09-23 01:31:53 +0900

    Install font-awesome

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,21 @@
 <footer>
-  <p class="copy-right">thank you for visiting me! feel free to contact me</p>
-  <a href="https://yohtanaka.github.io/myprofile/">
-    <p class="copy-right">Yohei Tanaka's Profile</p>
-  </a>
-  <p class="copy-right">&copy; 2018 Yohei Tanaka</p>
+	<p>thank you for visiting me! feel free to contact me</p>
+	<a href="https://yohtanaka.github.io/myprofile/">
+		<p>Yohei Tanaka's Profile</p>
+	</a>
+	<div class="center">
+		<a href="https://twitter.com/yohei_tanaka_">
+			<i class="fa fa-twitter"></i>Twitter
+		</a>
+		<a href="https://www.instagram.com/yoheitanakayohei/">
+			<i class="fa fa-instagram"></i>Instagram
+		</a>
+		<a href="https://www.facebook.com/yohei.tanaka.900">
+			<i class="fa fa-facebook"></i>Facebook
+		</a>
+		<a href="https://www.facebook.com/yohei.tanaka.900">
+			<i class="fa fa-github"></i>Github
+		</a>
+	</div>
+	<p>&copy; 2018 Yohei Tanaka</p>
 </footer>
